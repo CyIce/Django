ad9a722b574aae973ec3ad577caab5332935e1dc
cyice <1207201395@qq.com>
2018-08-02 14:35:47 +0800

    提取requirements.txt文件。

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/cyice/Documents/Django/Django/venv"
+set -gx VIRTUAL_ENV "/Users/cyice/Documents/Django/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
